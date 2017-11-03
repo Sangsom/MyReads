9b6afa91829b50d7636589a77a92baeb6bb5795a
Rinalds Pudulis <rpudulis@gmail.com>
2017-11-03 14:48:54 +0200

    feat: Added search Link component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
 import { SearchBooks } from './SearchBooks';
 import { Bookshelf } from './Bookshelf';
@@ -74,7 +75,7 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={this.handleSearchPage}>Add a book</a>
+              <Link to="/search">Add a book</Link>
             </div>
           </div>
         )}
