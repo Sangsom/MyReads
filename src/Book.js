c8a603a82ecdbae02da1d9ee92f280a3bf650796
Rinalds Pudulis <rpudulis@gmail.com>
2017-11-03 14:49:53 +0200

    refactor: Removed import Component call becaouse of warning

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ShelfChanger } from './ShelfChanger';
 
 export const Book = (props) => {
--- src/ShelfChanger.js
+++ src/ShelfChanger.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 export const ShelfChanger = (props) => {
     const {book, moveBook} = props;
