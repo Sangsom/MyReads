eb5e0a982ab4b476ac8a7f1ce826b7d273882e3b
Rinalds Pudulis <rpudulis@gmail.com>
2017-11-03 15:13:07 +0200

    feat: Routes are used now instead of links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
 import { SearchBooks } from './SearchBooks';
 import { Bookshelf } from './Bookshelf';
@@ -13,18 +13,11 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false,
     books: []
   }
 
   componentDidMount() {
     BooksAPI.getAll().then(books => this.setState({ books }));
-  }
-
-  handleSearchPage = () => {
-    this.setState((prevState) => ({
-      showSearchPage: !prevState.showSearchPage
-    }))
   }
 
   moveBook = (book, shelf) => {
@@ -45,40 +38,40 @@ class BooksApp extends React.Component {
 
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <SearchBooks handleSearchPage={this.handleSearchPage} />
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Bookshelf 
-                  books={this.state.books} 
-                  shelf='currentlyReading'
-                  title={bookShelfs.currentlyReading}
-                  moveBook={this.moveBook} 
-                />
-                <Bookshelf 
-                  books={this.state.books} 
-                  shelf='wantToRead'
-                  title={bookShelfs.wantToRead}
-                  moveBook={this.moveBook}
-                />
-                <Bookshelf 
-                  books={this.state.books} 
-                  shelf='read'
-                  title={bookShelfs.read}
-                  moveBook={this.moveBook}
-                />
+          <Route exact path="/" render={() => (
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <Bookshelf 
+                    books={this.state.books} 
+                    shelf='currentlyReading'
+                    title={bookShelfs.currentlyReading}
+                    moveBook={this.moveBook} 
+                  />
+                  <Bookshelf 
+                    books={this.state.books} 
+                    shelf='wantToRead'
+                    title={bookShelfs.wantToRead}
+                    moveBook={this.moveBook}
+                  />
+                  <Bookshelf 
+                    books={this.state.books} 
+                    shelf='read'
+                    title={bookShelfs.read}
+                    moveBook={this.moveBook}
+                  />
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to="/search">Add a book</Link>
               </div>
             </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
-            </div>
-          </div>
-        )}
+          )} />
+
+          <Route path="/search" component={SearchBooks} />
       </div>
     )
   }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class SearchBooks extends Component {
     render() {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                <a className="close-search" onClick={this.props.handleSearchPage}>Close</a>
+                <Link   
+                    to="/"
+                    className="close-search">Close</Link>
                 <div className="search-books-input-wrapper">
                     {/*
                     NOTES: The search from BooksAPI is limited to a particular set of search terms.
