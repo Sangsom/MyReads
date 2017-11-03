222e223ca5dcb3bc8b57f1585fc8d0586c37e7d8
Rinalds Pudulis <rpudulis@gmail.com>
2017-11-03 09:35:38 +0200

    refactor: Book component refactored to stateless component

--- src/Book.js
+++ src/Book.js
@@ -1,29 +1,27 @@
 import React, { Component } from 'react';
 import { ShelfChanger } from './ShelfChanger';
 
-export class Book extends Component {
-    render() {
-        const book = this.props.book;
-        return (
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover" 
-                        style={
-                            { 
-                                width: 128, 
-                                height: 193, 
-                                backgroundImage: `url("${book.imageLinks.thumbnail}")`
-                            }
-                        }>
-                    </div>
-                    <ShelfChanger 
-                        book={book} 
-                        moveBook={this.props.moveBook} 
-                    />
+export const Book = (props) => {
+    const {book, moveBook} = props;
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" 
+                    style={
+                        { 
+                            width: 128, 
+                            height: 193, 
+                            backgroundImage: `url("${book.imageLinks.thumbnail}")`
+                        }
+                    }>
                 </div>
-                <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors}</div>
+                <ShelfChanger 
+                    book={book} 
+                    moveBook={moveBook} 
+                />
             </div>
-        );
-    }
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors}</div>
+        </div>
+    );
 }
