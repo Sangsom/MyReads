00b2ce5910fe64465965f6eef34d5894491d16a3
Rinalds Pudulis <rpudulis@gmail.com>
2017-11-14 09:48:11 +0200

    refactor: Changed Bookshelf key to book id

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -11,8 +11,8 @@ export const Bookshelf = (props) => {
             <h2 className="bookshelf-title">{title}</h2>
             <div className="bookshelf-books">
                 <ol className="books-grid">
-                    { books.map((book, index) => (
-                        <li key={index}>
+                    { books.map(book => (
+                        <li key={book.id}>
                             <Book book={book} moveBook={moveBook} />
                         </li>
                     ))}
