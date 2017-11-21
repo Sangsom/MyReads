fbb4ee7193e65c4a7c7eb3d722eb0b29a4aaf32e
Rinalds Pudulis <rpudulis@gmail.com>
2017-11-21 18:56:06 +0200

    style: Styled error page

--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Header, Button } from 'semantic-ui-react';
 
 export const ErrorPage = (props) => {
+    const style = {
+        paddingTop: '10%'
+    }
+
     return (
-        <p>Hello Error</p>
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <Grid centered columns={2}>
+                <Grid.Row>
+                    <Grid.Column textAlign='center'>
+                        <Header style={style} as='h1' color='red'>There is no such page. Press return to go back.</Header>
+                        <Link to="/">
+                            <Button secondary>Return</Button>
+                        </Link>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </div>
     )
 }
